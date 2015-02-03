9ef44547527dca69dd7e4a6e6e1bd3becf8ad7c7
tomheng <zhm20070928@gmail.com>
2015-02-03 15:24:22 +0800

    add errror redirect

--- example/svnup.php
+++ example/svnup.php
@@ -7,7 +7,7 @@
  *
  */
 header("Cache-Control:no-cache,must-revalidate");  
-$handle = popen('export LC_CTYPE=en_US.UTF-8 && /usr/bin/svn up --username user_test --password pass_test /data1/svn_repo', 'r');  
+$handle = popen('export LC_CTYPE=en_US.UTF-8 && /usr/bin/svn up --username user_test --password pass_test /data1/svn_repo 2>&1', 'r');  
 $read = stream_get_contents($handle);//需要 PHP5 或更高版本  
 echo "<pre>";  
 printf($read);  

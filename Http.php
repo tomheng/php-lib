47f19e76ee842b2995f5621f73b87dc6170c3f73
tomheng <zhm20070928@gmail.com>
2015-03-12 15:45:34 +0800

    socket request

--- Http.php
+++ Http.php
@@ -8,37 +8,50 @@
 class Http
 {
 	/**
-	 * 异步POST请求
+	 * SOCKET 请求
 	 *
 	 * @return void
 	 * @author Heng Min Zhan
 	 */
-	public static function ($url, $params)
+	public static function socketRequest($url, $params, $wait_result = true, $connect_timeout = 3)
 	{
-	    foreach ($params as $key => &$val) {
-	      if (is_array($val)) $val = implode(',', $val);
-	        $post_params[] = $key.'='.urlencode($val);
-	    }
-	    $post_string = implode('&', $post_params);
-
+		$method = 'GET';
+		if($params){
+			$method = 'POST';
+			if(is_array($params)){
+				$post_string = http_build_query($params, '', '&');
+			}else{
+				$post_string = trim($params);
+			}
+		}
 	    $parts=parse_url($url);
-
-	    $fp = fsockopen($parts['host'],
-	        isset($parts['port'])?$parts['port']:80,
-	        $errno, $errstr, 3);
+		if(isset($parts['port'])){
+			$port =	$parts['port'];
+		}else{
+			$port = 80;
+		}
+	    $fp = fsockopen($parts['host'], $port, $errno, $errstr, $connect_timeout);
 	    if(!$fp)
 	    {
-	    	return false;
+			throw new Exception($errstr, $errno);
 	    }
-	    $out = "POST ".$parts['path']." HTTP/1.1\r\n";
-	    $out.= "Host: ".$parts['host']."\r\n";
+	    $out = "$method {$parts['path']} HTTP/1.1\r\n";
+	    $out.= "Host: {$parts['host']}\r\n";
 	    $out.= "Content-Type: application/x-www-form-urlencoded\r\n";
-	    $out.= "Content-Length: ".strlen($post_string)."\r\n";
 	    $out.= "Connection: Close\r\n\r\n";
-	    if (isset($post_string)) $out.= $post_string;
-
+		if (isset($post_string)) {
+			$out.= $post_string;
+			$out.= "Content-Length: ".strlen($post_string)."\r\n";
+		}
 	    fwrite($fp, $out);
-	    fclose($fp);
+		$result = '';
+		if($wait_result){
+			while (!feof($fp)) {
+				    $result .= fgets($fp, 1024);
+			}
+		}
+		fclose($fp);
+		return $result;
 	}
 	
 } // END class 

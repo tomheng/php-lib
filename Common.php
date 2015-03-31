df886d70a1a50670b983d46f29866dace6388927
tomheng <zhm20070928@gmail.com>
2015-03-31 12:07:03 +0800

    modify

--- Common.php
+++ Common.php
@@ -10,32 +10,32 @@ class  Common
 
 	//验证码生成
 	static public function showCaptcha($width = 100,  $height = 30,  $len){
-		$image = imagecreatetruecolor($width,  $height);//创建一个宽100，高度30的图片
+		$image = imagecreatetruecolor($width, $height);//创建一个宽100，高度30的图片
 		$bgcolor = imagecolorallocate($image, 255, 255, 255);//图片背景是白色
 		imagefill($image, 0, 0, $bgcolor);//图片填充白色
 		//随机数据，下面的例子是随机数据，包括字母和数字
 		$captch_code = '';
-		$fontsize  = $height * 0.75;
+		$font  = 5;
 		$data = '23456789abcdefghijkmnpqrstuvwxyz';
 		$data_len = strlen($data);
 		for($i = 0; $i < $len; $i++){
 			$fontcolor = imagecolorallocate($image, rand(0, 120), rand(0, 120), rand(0, 120));
 			$fontcontent = substr($data, rand(0, $data_len - 1), 1);
 			$captch_code .= $fontcontent;
-			$x = ($i*100/4)+ rand(5, 10);
-			$y = rand(5, 10);
-			imagestring($image, $fontsize, $x, $y, $fontcontent, $fontcolor);
+			$x = ($i * $width / $len) + rand(5, 8);
+			$y = rand(0, $height/3);
+			imagestring($image, $font, $x, $y, $fontcontent, $fontcolor);
 		}
-
 		//随机点，生成干扰点
-		for($i = 0;$i<200;$i++){
+		$pn = ($width * $height) * 0.1;
+		for($i = 0; $i < $pn; $i++){
 			$pointcolor = imagecolorallocate($image, rand(50, 120), rand(50, 120), rand(50, 120));
-			imagesetpixel($image, rand(1, 99), rand(1, 99), $pointcolor);
+			imagesetpixel($image, rand(1, $width), rand(1, $width), $pointcolor);
 		}
 		//随机线，生成干扰线
 		for($i = 0;$i<3;$i++){
 			$linecolor = imagecolorallocate($image, rand(80, 220), rand(80, 220), rand(80, 220));
-			imageline($image, rand(1, 99), rand(1, 29), rand(1, 99), rand(1, 29), $linecolor);
+			imageline($image, rand(1, $width), rand(1, $height), rand(1, $width), rand(1, $height), $linecolor);
 		}
 		header("content-type:image/png");
 		imagepng($image);

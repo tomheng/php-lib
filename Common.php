79d7544c94ad7577dec33ad1677600d14b555d23
tomheng <zhm20070928@gmail.com>
2015-03-31 11:32:31 +0800

    add captch function

--- Common.php
+++ Common.php
@@ -5,47 +5,83 @@
  * @package default
  * @author Heng Min Zhan
  */
-class 
+class  Common
 {
-    /**
-     * 转码函数
-     * @param Mixed $data 需要转码的数组
-     * @param String $dstEncoding 输出编码
-     * @param String $srcEncoding 传入编码
-     * @param bool $toArray 是否将stdObject转为数组输出
-     * @return Mixed
-     */
-    static public function convertEncoding($data, $dstEncoding, $srcEncoding, $toArray=false) {
-        if ($toArray && is_object($data)) {
-            $data = (array)$data;
-        }
-        if (!is_array($data) && !is_object($data)) {
-            $data = mb_convert_encoding($data, $dstEncoding, $srcEncoding);
-        } else {
-            if (is_array($data)) {
-                foreach($data as $key=>$value) {
-                    if (is_numeric($value)) {
-                        continue;
-                    }
-                    $keyDstEncoding = self::convertEncoding($key, $dstEncoding, $srcEncoding, $toArray);
-                    $valueDstEncoding = self::convertEncoding($value, $dstEncoding, $srcEncoding, $toArray);
-                    unset($data[$key]);
-                    $data[$keyDstEncoding] = $valueDstEncoding;
-                }
-            } else if(is_object($data)) {
-                $dataVars = get_object_vars($data);
-                foreach($dataVars as $key=>$value) {
-                    if (is_numeric($value)) {
-                        continue;
-                    }
-                    $keyDstEncoding = self::convertEncoding($key, $dstEncoding, $srcEncoding, $toArray);
-                    $valueDstEncoding = self::convertEncoding($value, $dstEncoding, $srcEncoding, $toArray);
-                    unset($data->$key);
-                    $data->$keyDstEncoding = $valueDstEncoding;
-                }
-            }
-        }
-        return $data;
-    }
-	
+
+	//验证码生成
+	static public function showCaptcha($width = 100,  $height = 30,  $len){
+		$image = imagecreatetruecolor($width,  $height);//创建一个宽100，高度30的图片
+		$bgcolor = imagecolorallocate($image, 255, 255, 255);//图片背景是白色
+		imagefill($image, 0, 0, $bgcolor);//图片填充白色
+		//随机数据，下面的例子是随机数据，包括字母和数字
+		$captch_code = '';
+		$fontsize  = $height * 0.75;
+		$data = '23456789abcdefghijkmnpqrstuvwxyz';
+		$data_len = strlen($data);
+		for($i = 0; $i < $len; $i++){
+			$fontcolor = imagecolorallocate($image, rand(0, 120), rand(0, 120), rand(0, 120));
+			$fontcontent = substr($data, rand(0, $data_len - 1), 1);
+			$captch_code .= $fontcontent;
+			$x = ($i*100/4)+ rand(5, 10);
+			$y = rand(5, 10);
+			imagestring($image, $fontsize, $x, $y, $fontcontent, $fontcolor);
+		}
+
+		//随机点，生成干扰点
+		for($i = 0;$i<200;$i++){
+			$pointcolor = imagecolorallocate($image, rand(50, 120), rand(50, 120), rand(50, 120));
+			imagesetpixel($image, rand(1, 99), rand(1, 99), $pointcolor);
+		}
+		//随机线，生成干扰线
+		for($i = 0;$i<3;$i++){
+			$linecolor = imagecolorallocate($image, rand(80, 220), rand(80, 220), rand(80, 220));
+			imageline($image, rand(1, 99), rand(1, 29), rand(1, 99), rand(1, 29), $linecolor);
+		}
+		header("content-type:image/png");
+		imagepng($image);
+		return $captch_code;
+	}
+
+
+	/**
+	 * 转码函数
+	 * @param Mixed $data 需要转码的数组
+	 * @param String $dstEncoding 输出编码
+	 * @param String $srcEncoding 传入编码
+	 * @param bool $toArray 是否将stdObject转为数组输出
+	 * @return Mixed
+	 */
+	static public function convertEncoding($data, $dstEncoding, $srcEncoding, $toArray = false) {
+		if ($toArray && is_object($data)) {
+			$data = (array)$data;
+		}
+		if (!is_array($data) && !is_object($data)) {
+			$data = mb_convert_encoding($data, $dstEncoding, $srcEncoding);
+		} else {
+			if (is_array($data)) {
+				foreach($data as $key=>$value) {
+					if (is_numeric($value)) {
+						continue;
+					}
+					$keyDstEncoding = self::convertEncoding($key, $dstEncoding, $srcEncoding, $toArray);
+					$valueDstEncoding = self::convertEncoding($value, $dstEncoding, $srcEncoding, $toArray);
+					unset($data[$key]);
+					$data[$keyDstEncoding] = $valueDstEncoding;
+				}
+			} else if(is_object($data)) {
+				$dataVars = get_object_vars($data);
+				foreach($dataVars as $key=>$value) {
+					if (is_numeric($value)) {
+						continue;
+					}
+					$keyDstEncoding = self::convertEncoding($key, $dstEncoding, $srcEncoding, $toArray);
+					$valueDstEncoding = self::convertEncoding($value, $dstEncoding, $srcEncoding, $toArray);
+					unset($data->$key);
+					$data->$keyDstEncoding = $valueDstEncoding;
+				}
+			}
+		}
+		return $data;
+	}
+
 } // END class 
